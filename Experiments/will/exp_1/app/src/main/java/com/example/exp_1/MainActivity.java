22426391ebc100375200a203ec9ce3541f2855c9
wasartin@iastate.edu <wasartin@iastate.edu>
2019-01-24 13:56:10 -0600

    Learning AS

--- Experiments/will/exp_1/app/src/main/java/com/example/exp_1/MainActivity.java
+++ Experiments/will/exp_1/app/src/main/java/com/example/exp_1/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.exp_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.buttonMessage);
+        b2 = (Button) findViewById(R.id.buttonNext);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "Hello Android !", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }

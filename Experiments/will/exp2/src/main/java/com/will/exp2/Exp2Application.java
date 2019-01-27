5a7167a2f00908d30959aba19a2eecdb62a04aae
wasartin@iastate.edu <wasartin@iastate.edu>
2019-01-27 13:58:15 -0600

    working with Lynda examples for SpringBoot

---
+++ Experiments/will/exp2/src/main/java/com/will/exp2/Exp2Application.java
@@ -0,0 +1,14 @@
+package com.will.exp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp2Application.class, args);
+	}
+
+}
+
---
+++ Experiments/will/exp2/src/test/java/com/will/exp2/Exp2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.will.exp2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Exp2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

b86026b2e19e08d28222779a1b84871979e5e7f6
Ben Pierre <newunusedname@gmail.com>
2019-01-27 00:07:06 -0600

    Bug Fixes

--- Experiments/Ben/FoodsList/app/src/main/java/android/example/foodslist/MainActivity.java
+++ Experiments/Ben/FoodsList/app/src/main/java/android/example/foodslist/MainActivity.java
@@ -2,18 +2,25 @@ package android.example.foodslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView FoodTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
-        String[] Resturants = FoodList.getFoodPlaces();
+        FoodTextView = (TextView) findViewById(R.id.tv_toy_names);
+        String[] Food = FoodList.getFoodPlaces();
 
-        for(String location " Resturants")
-            
+        for(String location : Food){
+            FoodTextView.append(location + "\n\n\n");
+        }
+
+        
+
     }
 }

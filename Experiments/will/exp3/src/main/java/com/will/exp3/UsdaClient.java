0b53a1f30120e08973bf9e6e43a7e9ff47e3abdc
wasartin@iastate.edu <wasartin@iastate.edu>
2019-01-31 09:46:30 -0600

    working with API calls

---
+++ Experiments/will/exp3/src/main/java/com/will/exp3/UsdaClient.java
@@ -0,0 +1,20 @@
+package com.will.exp3;
+
+import org.json.simple.JSONObject;
+
+public class UsdaClient {
+
+	private final String API_KEY = "pd4EQsX2gq4jS3GAkUYcaqM6u8w5ylbB56MFlTQ4";
+	
+	private final String CONTENT_TYPE = "application/json";
+	
+	
+	public UsdaClient() {
+		
+	}
+	
+	public JSONObject getResponse() {
+		
+		return null;
+	}
+}

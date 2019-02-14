862bbb7088e158185448fb6d73d9c9a35228134e
wasartin@iastate.edu <wasartin@iastate.edu>
2019-02-14 10:04:46 -0600

    temp User controller

--- Backend/business/src/main/java/entities/controller/UserController.java
+++ Backend/business/src/main/java/entities/controller/UserController.java
@@ -1,13 +1,10 @@
 package entities.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import entities.data.User;
@@ -16,20 +13,17 @@ import entities.repository.UserRepository;
 @RequestMapping("users")
 @RestController
 public class UserController {
+	@Autowired
 	private final UserRepository userRepo;
 	
 	UserController(UserRepository userRepo){
 		this.userRepo = userRepo;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	@ResponseBody
-	public Map<String, Object> createUser(@RequestBody User user){
-		Map<String, Object> response = new HashMap<>();
-		//Optional<User> temp = userRepo.findByEmail(user.getEmail());
-		//TODO
-		return response;
-	}
+	//@GetMapping("users")
+	//List<User> get(){
+	//	return userService.getAll();
+	//}
 	
 		
 	

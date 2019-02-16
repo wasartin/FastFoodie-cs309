a297676626c93ae517cfe6cfa48bb4970690f701
wasartin@iastate.edu <wasartin@iastate.edu>
2019-02-16 15:23:41 -0600

    can now create user through HTTP.POST

--- Backend/business/src/main/java/com/example/business/data/repositories/UserRepository.java
+++ Backend/business/src/main/java/com/example/business/data/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.business.data.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.business.data.entities.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,Integer>{
+public interface UserRepository extends CrudRepository<User,String>{
 	
+	//@Query(value = "select * from user where email = ?1", nativeQuery = true)
+	//List<User> findByID(String email);
+
 }

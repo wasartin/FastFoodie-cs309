52900d461258091132306bd0c858b240c2a8081a
jgreazel <jgreazel@iastate.edu>
2019-02-24 15:06:29 -0600

    refactoring controller

--- Backend/business/src/main/java/com/example/business/data/controllers/RestaurantController.java
+++ Backend/business/src/main/java/com/example/business/data/controllers/RestaurantController.java
@@ -1,12 +1,17 @@
 package com.example.business.data.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,23 +29,69 @@ public class RestaurantController {
 	@Autowired
 	RestaurantRepository restaurantRepo;
 	
-	@GetMapping("/all")
-	public Iterable<Restaurant> getAllRestaurants(){
-		return restaurantRepo.findAll();
+	/**
+	 * 
+	 * @return JSONObject that has key1-> "Restaurants": value1->JSONArray of restaurants in System
+	 * @throws JSONException 
+	 */
+	@RequestMapping(value ="/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public JSONObject getAllRestaurantsJSONObject() throws JSONException {
+		JSONObject toReturn = new JSONObject();
+		String key1 = "Restaurants";
+		JSONArray listOfRestaurants = new JSONArray();
+		List<Restaurant> rList = getRestaurants();
+		for(int i = 0; i < rList.size(); i++) {
+			JSONObject temp = new JSONObject();
+			temp.put("id", rList.get(i).getRestaurant_id());
+			temp.put("name", rList.get(i).getRestaurant_name());
+			temp.put("last_updated", rList.get(i).getLast_updated());
+			listOfRestaurants.put(temp);
+		}
+		toReturn.put(key1, listOfRestaurants);
+		return toReturn;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/{restaurant_id}")
+	/**
+	 * used to find list of all restaurants
+	 * @return List of all restaurants
+	 */
+	private List<Restaurant> getRestaurants(){
+		Iterable<Restaurant> rIters = restaurantRepo.findAll();
+		List<Restaurant> rList = new ArrayList<Restaurant>();
+		rIters.forEach(rList::add);
+		return rList;
+	}
+
+	/**
+	 * returns JSON Object of restaurant whose id was specified
+	 * @param restaurant_id id of desired restaurant
+	 * @return JSON Object of desired restaurant
+	 * @throws JSONException
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/{restaurant_id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public Optional<Restaurant> getRestaurant(@PathVariable String restaurant_id){
-		return restaurantRepo.findById(restaurant_id);
+	public JSONObject getUserJSONObject(@PathVariable int restaurant_id) throws JSONException {
+		Optional<Restaurant> temp = restaurantRepo.findById(restaurant_id);
+		JSONObject toReturn = new JSONObject();
+		JSONObject restaurantInfo = new JSONObject();
+		restaurantInfo.put("id", temp.get().getRestaurant_id());
+		restaurantInfo.put("name", temp.get().getRestaurant_name());
+		restaurantInfo.put("last_updated", temp.get().getLast_updated());
+		toReturn.put("User", restaurantInfo);
+		return toReturn;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, path = "/create")
+
+	/**
+	 * adds a new restaurant to the database if said restaurant doesn't already exist
+	 * @param newRestaurant
+	 * @return Map of the message and http status of the post
+	 */
+	@RequestMapping(method = RequestMethod.POST, path = "/create", produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	private Map<String, Object> createRestaurant(@RequestBody Restaurant newRestaurant){
 		HashMap<String, Object> response = new HashMap<>();
 		try {
-			if(restaurantRepo.findById(newRestaurant.getRestaurant_id())==null)
+			if(restaurantRepo.existsById(newRestaurant.getRestaurant_id()))
 				throw new IllegalArgumentException();	
 			restaurantRepo.save(newRestaurant);
 			response.put("status", 200);
@@ -57,12 +108,12 @@ public class RestaurantController {
 		return response;
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{restaurant_id}")
+	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{restaurant_id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	private Map<String,Object> deleteRestaurant(@PathVariable String restaurant_id ) {
+	private Map<String,Object> deleteRestaurant(@PathVariable int restaurant_id ) {
 		HashMap<String,Object> response = new HashMap<>();
 		try {
-			if(restaurantRepo.findById(restaurant_id) == null)//Checks to see if User is even in the DB
+			if(!restaurantRepo.existsById(restaurant_id))//Checks to see if User is even in the DB
 				throw new IllegalArgumentException();
 			restaurantRepo.deleteById(restaurant_id);
 			response.put("status", 200);
--- Backend/business/src/main/resources/restaurantSchema.sql
+++ Backend/business/src/main/resources/restaurantSchema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE restaurant(
-	restaurant_id VARCHAR(10) NOT NULL,
+	restaurant_id INT NOT NULL,
 	restaurant_name VARCHAR(50),
 	last_updated DATE,
 	PRIMARY KEY(restaurant_id)

acf2448be2ca774fc949ba93d06e78c610b24c11
Ben Pierre <newunusedname@gmail.com>
2019-04-02 17:09:43 -0500

    Done, Just needs database integration

--- Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/PopUps/submitPopUp.java
+++ Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/PopUps/submitPopUp.java
@@ -3,11 +3,14 @@ package edu.iastate.graysonc.fastfood.PopUps;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.iastate.graysonc.fastfood.R;
 import edu.iastate.graysonc.fastfood.database.entities.Ticket;
@@ -24,7 +27,6 @@ public class submitPopUp extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ticketpopup);
         mSubmit = findViewById(R.id.SubmitButton);
-
         mQueryInput = findViewById(R.id.QueryInputBox);
         mSpinner = findViewById(R.id.spinner);
 
@@ -34,9 +36,17 @@ public class submitPopUp extends Activity {
 
         mSubmit.setOnClickListener(View -> {
             //TODO I can't figure out how to inject stuff here rip, so I can't get the users Id
-            //ticket = new Ticket("",mQueryInput.getText().toString(),mSpinner.toString());
+            ticket = new Ticket("",mQueryInput.getText().toString().replaceAll("\n"," "),mSpinner.getSelectedItem().toString());
             Toast.makeText(this,"Thank you for your input, we'll be back to you soon!", Toast.LENGTH_LONG).show();
             this.finish();
         });
+
+
+        List<String> categories = new ArrayList<>();
+        categories.add("Contact us");
+        categories.add("Error");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(dataAdapter);
     }
 }

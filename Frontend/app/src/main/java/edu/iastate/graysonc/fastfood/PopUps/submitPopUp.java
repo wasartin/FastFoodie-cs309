c6d7c27f1d60f8fb9e4bf8c9a3480c756b96508f
Ben Pierre <newunusedname@gmail.com>
2019-04-01 19:53:56 -0500

    Done minus populating spinner and database integration

--- Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/PopUps/submitPopUp.java
+++ Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/PopUps/submitPopUp.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import edu.iastate.graysonc.fastfood.R;
 import edu.iastate.graysonc.fastfood.database.entities.Ticket;
 
-public class submitPopUp extends Activity implements View.OnClickListener {
+public class submitPopUp extends Activity {
    private Button mSubmit;
    private EditText mQueryInput;
    private Spinner mSpinner;
@@ -21,23 +22,21 @@ public class submitPopUp extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.ticketpopup);
         mSubmit = findViewById(R.id.SubmitButton);
-        mSubmit.setOnClickListener(this);
+
         mQueryInput = findViewById(R.id.QueryInputBox);
         mSpinner = findViewById(R.id.spinner);
-        setContentView(R.layout.ticketpopup);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-
         getWindow().setLayout((int) (dm.widthPixels *.95),(int)(dm.heightPixels *.85));
-    }
 
-
-    @Override
-    public void onClick(View v) {
-
+        mSubmit.setOnClickListener(View -> {
+            //TODO I can't figure out how to inject stuff here rip, so I can't get the users Id
+            //ticket = new Ticket("",mQueryInput.getText().toString(),mSpinner.toString());
+            Toast.makeText(this,"Thank you for your input, we'll be back to you soon!", Toast.LENGTH_LONG).show();
+            this.finish();
+        });
     }
 }
--- Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/database/entities/Ticket.java
+++ Frontend/app/src/main/java/edu/iastate/graysonc/fastfood/database/entities/Ticket.java
@@ -1,3 +1,34 @@
 package edu.iastate.graysonc.fastfood.database.entities;
 
-public class Ticket {}
+import java.util.Date;
+
+public class Ticket {
+    private String issue;
+    private String userID;
+    private Date date;
+    private String category;
+
+    public Ticket(String email, String issue, String category){
+        userID = email;
+        this.issue = issue;
+        this.category = category;
+        date = new Date();
+    }
+
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}

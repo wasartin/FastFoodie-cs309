13ef0852764dd265b1eae06881874b07af9e7597
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-04 10:32:33 -0500

    Init websockets

---
+++ Backend/business/src/main/java/websocket/WebSocketServer.java
@@ -0,0 +1,14 @@
+package websocket;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.websocket.Session;
+
+public class WebSocketServer {
+
+	private static Map<Session, String> sessionUserNameMap = new HashMap<>();
+	private static Map<String, Session> usernameSessionMap = new HashMap<>();
+	
+	
+}

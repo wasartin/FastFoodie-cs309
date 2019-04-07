c09e10b612516a2003be6df243efd5d7785f7ded
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-07 17:04:49 -0500

    test error possibly

--- Backend/business/src/main/java/com/example/business/websocket/WebSocketServer.java
+++ Backend/business/src/main/java/com/example/business/websocket/WebSocketServer.java
@@ -15,11 +15,13 @@ import javax.websocket.server.ServerEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.example.business.data.entities.FoodRating;
 import com.example.business.data.repositories.FoodRatingRepository;
 
 @ServerEndpoint("/websocket/{user_email}")
+@Component
 public class WebSocketServer {
 	@Autowired
 	FoodRatingRepository foodRatingRepo;

29620127cd296accce2e9cf9235e49f9540be4dc
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-18 14:26:18 -0500

    local DB loaded, eveything looks good. Going to start testing

---
+++ Backend/business/src/main/resources/ticketData.sql
@@ -0,0 +1,3 @@
+INSERT INTO ticket(user_id, admin_id, text, category) VALUES
+	('TomDodge@gmail.com', 'wasartin@iastate.edu', 'My Tom Dodge just is not as Tom Dodge as it used to be', 'personalProblems'),
+    ('hughMan@planetexpress.com', 'newunusedname@gmail.com', 'None of the McDonalds I have been have a Royal with Cheese. AM I doing something wrong?', 'food');

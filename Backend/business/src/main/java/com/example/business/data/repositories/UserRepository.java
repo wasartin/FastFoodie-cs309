382645691b767280f4c9a1ef7dec51ac5767d011
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-20 17:05:09 -0500

    more negative coe for user

--- Backend/business/src/main/java/com/example/business/data/repositories/UserRepository.java
+++ Backend/business/src/main/java/com/example/business/data/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.business.data.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +7,5 @@ import com.example.business.data.entities.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User,String>{
-	
-	@Query(value = "select * from user where email = ?1", nativeQuery = true)
-	User findByID(String email);
+
 }

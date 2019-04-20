dd1586e4179443f317fa87f9215212f7f1b04914
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-20 12:26:33 -0500

    Checkpoint: Pre-test of get, getall, and create on FoodService

--- Backend/business/src/main/java/com/example/business/data/services/AbstractService.java
+++ Backend/business/src/main/java/com/example/business/data/services/AbstractService.java
@@ -8,10 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public abstract class AbstractService<E, K> {
-
-	private static final String MESSAGE_SUCCESS = "%s has been created";
-	private static final String MESSAGE_FAIL_THEIR_FAULT = "%s might already exist, or your fields are incorrect, double check your request";
-	private static final String MESSAGE_FAIL_OUR_FAULT = "Server might be down now. Try again";
 	
 	private CrudRepository<E, K> repo;
 	
@@ -23,41 +19,21 @@ public abstract class AbstractService<E, K> {
 		return repo.findAll();
 	}
 	
-//	//TODO
-//	public ResponseEntity<?> createEntity(E newEntity, K id){
-//		JSONObject response;
-//		try {
-//			if(repo.existsById(id)) {
-//				throw new IllegalArgumentException();
-//			}
-//			repo.save(newEntity);
-//			response = generateResponse(HttpStatus.OK, newEntity.getClass().toString());
-//		}catch (IllegalArgumentException e) {
-//			response = generateResponse(HttpStatus.BAD_REQUEST, );
-//			return new ResponseEntity.badRequest().body(newEntity.getClass().toString());
-//		}catch (Exception e) {
-//			response = generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, newEntity.getClass().toString());
-//		}
-//		return new ResponseEntity<>();
-//	}
+	public ResponseEntity<?> createEntity(E newEntity, K id){
+		ResponseEntity<?> response;
+		String className = newEntity.getClass().getSimpleName();
+		try {
+			if(repo.existsById(id)) {
+				throw new IllegalArgumentException();
+			}
+			repo.save(newEntity);
+			response =  ResponseEntity.status(HttpStatus.OK).body(className);
+		}catch (IllegalArgumentException e) {
+			response =  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(className);
+		}catch (Exception e) {
+			response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(className);
+		}
+		return response;
+	}
 
-//	@SuppressWarnings("unchecked")
-//	final ResponseEntity<?> generateResponse(HttpStatus state, String keyword) {
-//		ResponseEntity response = null;
-//		String responseMessage = "";
-//		switch(state) {
-//		case OK:
-//			
-//			responseMessage = String.format(MESSAGE_SUCCESS, keyword);
-//			break;
-//		case BAD_REQUEST:
-//			responseMessage = String.format(MESSAGE_FAIL_THEIR_FAULT, keyword);
-//			break;
-//		case INTERNAL_SERVER_ERROR:
-//		default :
-//			responseMessage = String.format(MESSAGE_FAIL_OUR_FAULT);
-//			break;
-//		}
-//		response = new
-//	}
 }

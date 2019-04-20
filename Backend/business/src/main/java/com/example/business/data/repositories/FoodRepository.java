b4734b08b4cfb36eb830f565954e5e296e74fbb5
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-20 16:39:42 -0500

    AbstractService complelety works for the FoodController. FoodService doesn't have anything in it now, except setting the generics

--- Backend/business/src/main/java/com/example/business/data/repositories/FoodRepository.java
+++ Backend/business/src/main/java/com/example/business/data/repositories/FoodRepository.java
@@ -1,11 +1,14 @@
 package com.example.business.data.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.business.data.entities.Food;
+import com.example.business.data.entities.Ticket;
 
 @Repository
 public interface FoodRepository extends CrudRepository<Food, Integer>{
-	
 }
--- Backend/business/src/main/java/com/example/business/data/services/AbstractService.java
+++ Backend/business/src/main/java/com/example/business/data/services/AbstractService.java
@@ -2,38 +2,116 @@ package com.example.business.data.services;
 
 import java.util.Optional;
 
-import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 
+/**
+ * 
+ * @author watis
+ *
+ * @param <E> The DAO
+ * @param <K> The type of id this DAO uses.
+ */
+@Repository
 public abstract class AbstractService<E, K> {
 	
+	@Autowired
 	private CrudRepository<E, K> repo;
 	
+	private HttpHeaders headers = new HttpHeaders();
+
+	/**
+	 * Empty Constructor to init the HttpHeaders content type.
+	 */
+	public AbstractService() {
+		headers.setContentType(MediaType.APPLICATION_JSON);
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public Optional<E> getEntityByID(K id) {
 		return repo.findById(id);
 	}
 	
+	/**
+	 * 
+	 * @return a list of all entities in DB
+	 */
 	public Iterable<E> getAllEntities(){
 		return repo.findAll();
 	}
 	
+	/**
+	 * 
+	 * @param newEntity
+	 * @param id
+	 * @return
+	 */
 	public ResponseEntity<?> createEntity(E newEntity, K id){
-		ResponseEntity<?> response;
 		String className = newEntity.getClass().getSimpleName();
+		HttpStatus resultingStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 		try {
 			if(repo.existsById(id)) {
 				throw new IllegalArgumentException();
 			}
 			repo.save(newEntity);
-			response =  ResponseEntity.status(HttpStatus.OK).body(className);
+			resultingStatus = HttpStatus.OK;
 		}catch (IllegalArgumentException e) {
-			response =  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(className);
+			resultingStatus = HttpStatus.BAD_REQUEST;
 		}catch (Exception e) {
-			response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(className);
 		}
-		return response;
+		return new ResponseEntity<Object>(className, headers, resultingStatus);
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ResponseEntity<?> deleteEntity(K id){
+		HttpStatus resultingStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+		String className = "";
+		try {
+			if(!repo.existsById(id)) {
+				throw new IllegalArgumentException();
+			}
+			className = repo.findById(id).get().getClass().getSimpleName();
+			repo.deleteById(id);
+			resultingStatus = HttpStatus.OK;
+		}catch (IllegalArgumentException e) {
+			resultingStatus = HttpStatus.BAD_REQUEST;
+		}catch (Exception e) {
+		}
+		return new ResponseEntity<Object>(className, headers, resultingStatus);
 	}
 
+	/**
+	 * Editing an entity can only be done if the entity's id already exists in the DB. 
+	 * @param entityToEdit
+	 * @param id
+	 * @return
+	 */
+	public ResponseEntity<?> editEntity(E entityToEdit, K id){
+		HttpStatus resultingStatus;
+		String className = entityToEdit.getClass().getSimpleName();
+		try {
+			if(repo.existsById(id)) {
+				repo.save(entityToEdit);
+				resultingStatus = HttpStatus.OK;
+			}else {
+				resultingStatus = HttpStatus.BAD_REQUEST;
+			}
+		}catch (Exception e) {
+			resultingStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Object>(className, headers, resultingStatus);
+	}
 }

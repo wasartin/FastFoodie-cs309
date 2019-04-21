84f289cae7edaecabc3e6f69dfb158469454d976
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-20 20:42:59 -0500

    Set up Food Rating Service class

--- Backend/business/src/main/java/com/example/business/data/controllers/FoodRatingController.java
+++ Backend/business/src/main/java/com/example/business/data/controllers/FoodRatingController.java
@@ -2,10 +2,9 @@ package com.example.business.data.controllers;
 
 import java.util.List;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.business.data.entities.FoodRating;
-import com.example.business.data.repositories.FoodRatingRepository;
+import com.example.business.data.services.FoodRatingsService;
 
 /**
  * 
@@ -25,197 +24,128 @@ import com.example.business.data.repositories.FoodRatingRepository;
 @RequestMapping(value="/foodRatings")
 public class FoodRatingController {
 
-		@Autowired
-		FoodRatingRepository foodRatingRepo;
+	@Autowired
+	FoodRatingsService foodRatingService;
+	
+	/**
+	 * Uses Repo class to return all ratings for the foods in the DB
+	 * @return list of all Food Ratings
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/all")
+	@ResponseBody
+	public Iterable<FoodRating> getAll(){
+		return foodRatingService.getAllEntities();
+	}
+	
+	/**
+	 * retrieves a specific food rating with that rating's user email and food id
+	 * @param user_email
+	 * @param food_id
+	 * @return Specific food rating
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/{user_email}/{food_id}")
+	@ResponseBody
+	public FoodRating getSpecific(@PathVariable String user_email, @PathVariable int food_id){
+		return foodRatingService.getRatingForUserAndFood(user_email, food_id);
+	}
+	
+	/**
+	 * gets all food ratings for a specific user
+	 * @param user_email
+	 * @return list of all user's food ratings
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "all/{user_email}")
+	@ResponseBody
+	public List<FoodRating> getAllForUser(@PathVariable String user_email){
+		return foodRatingService.getAllRatingsByUser(user_email);
+	}
+	
+	/**
+	 * get all food ratings for a specific food
+	 * @param food_id
+	 * @return list of food ratings
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/all/{food_id}")
+	@ResponseBody
+	public List<Integer> getFoodRatingList(@PathVariable int food_id){
+		return foodRatingService.findAllRatingsForFood(food_id);
+	}
+	
+	/**
+	 * gets the average food ratings for a food, a great method for a lazy front end
+	 * @param food_id
+	 * @return food's rating
+	 */
+	@RequestMapping(method = RequestMethod.GET, path = "/average/{food_id}")
+	@ResponseBody
+	public double getFoodRating(@PathVariable int food_id){
+		List<Integer> ratingList = foodRatingService.findAllRatingsForFood(food_id);
+		 double sum = 0;
+		  if(!ratingList.isEmpty()) {
+		    for (int curr : ratingList) {
+		    	if (curr > 0 && curr < 6) {
+				       sum += curr;
+		    	}
+	
+		    }
+		  }
+		    return sum / ratingList.size();
+	}
+	
+	/**
+	 * create new food rating
+	 * @param newRating
+	 * @return a json object response
+	 */
+	@RequestMapping(method = RequestMethod.POST, path = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> createFoodRatingByJSON(@RequestBody FoodRating newRating) {
+		return foodRatingService.createEntity(newRating, newRating.getRating_id());
+	}
+	
+	/**
+	 * creates a new food rating by email, food id, and rating
+	 * @param user_email
+	 * @param food_id
+	 * @param rating
+	 * @return a json object response
+	 */
+	@RequestMapping(method = RequestMethod.POST, path = "/create/{user_email}/{food_id}/{rating}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> createFoodRating(@PathVariable String user_email, @PathVariable int food_id, @PathVariable int rating) {
+		FoodRating newRating = new FoodRating();
+		newRating.setFood_id(food_id);
+		newRating.setRating(rating);
+		newRating.setUser_email(user_email);
 		
-		/**
-		 * Uses Repo class to return all ratings for the foods in the DB
-		 * @return list of all Food Ratings
-		 */
-		@RequestMapping(method = RequestMethod.GET, path = "/all")
-		@ResponseBody
-		public List<FoodRating> getAll(){
-			return (List<FoodRating>) foodRatingRepo.findAll();
-		}
-		
-		/**
-		 * retrieves a specific food rating with that rating's user email and food id
-		 * @param user_email
-		 * @param food_id
-		 * @return Specific food rating
-		 */
-		@RequestMapping(method = RequestMethod.GET, path = "/{user_email}/{food_id}")
-		@ResponseBody
-		public FoodRating getSpecific(@PathVariable String user_email, @PathVariable int food_id){
-			return foodRatingRepo.getFoodRatingByUserAndFood(user_email, food_id);
-		}
-		
-		/**
-		 * gets all food ratings for a specific user
-		 * @param user_email
-		 * @return list of all user's food ratings
-		 */
-		@RequestMapping(method = RequestMethod.GET, path = "all/user/{user_email}")
-		@ResponseBody
-		public List<FoodRating> getAllForUser(@PathVariable String user_email){
-			return (List<FoodRating>) foodRatingRepo.getFoodRatingsForUser(user_email);
-		}
-		
-		/**
-		 * get all food ratings for a specific food
-		 * @param food_id
-		 * @return list of food ratings
-		 */
-		@RequestMapping(method = RequestMethod.GET, path = "/all/food/{food_id}")
-		@ResponseBody
-		public List<Integer> getFoodRatingList(@PathVariable int food_id){
-			return foodRatingRepo.findAllRatingsForFood(food_id);
-		}
-		
-		/**
-		 * gets the average food ratings for a food, a great method for a lazy front end
-		 * @param food_id
-		 * @return food's rating
-		 */
-		@RequestMapping(method = RequestMethod.GET, path = "/average/{food_id}")
-		@ResponseBody
-		public double getFoodRating(@PathVariable int food_id){
-			List<Integer> ratingList = foodRatingRepo.findAllRatingsForFood(food_id);
-			 double sum = 0;
-			  if(!ratingList.isEmpty()) {
-			    for (int curr : ratingList) {
-			    	if (curr > 0 && curr < 6) {
-					       sum += curr;
-			    	}
-
-			    }
-			  }
-			    return sum / ratingList.size();
-		}
-		
-		/**
-		 * create new food rating
-		 * @param newRating
-		 * @return a json object response
-		 */
-		@RequestMapping(method = RequestMethod.POST, path = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
-		@ResponseBody
-		public JSONObject createFoodRatingByJSON(@RequestBody FoodRating newRating) {
-			JSONObject response;
-			try {
-				if(foodRatingRepo.existsById(newRating.getRating_id())) {
-					throw new IllegalArgumentException();
-				}
-				foodRatingRepo.save(newRating);
-				response = generateResponse(204, HttpStatus.OK, "Food has bee rated");
-			}catch (IllegalArgumentException e) {
-				response = generateResponse(400, HttpStatus.BAD_REQUEST, "Rating might already exist, or your fields are incorrect, double check your request");
-			}catch (Exception e) {
-				response = generateResponse(500, HttpStatus.INTERNAL_SERVER_ERROR, "Server might be down now. Try again");
-			}
-			return response;
-		}
-		
-		/**
-		 * creates a new food rating by email, food id, and rating
-		 * @param user_email
-		 * @param food_id
-		 * @param rating
-		 * @return a json object response
-		 */
-		@RequestMapping(method = RequestMethod.POST, path = "/create/{user_email}/{food_id}/{rating}", produces = MediaType.APPLICATION_JSON_VALUE)
-		@ResponseBody
-		public JSONObject createFoodRating(@PathVariable String user_email, @PathVariable int food_id, @PathVariable int rating) {
-			if(rating < 0 || rating > 5) {
-				throw new IllegalArgumentException();
-			}
-			FoodRating newRating = new FoodRating();
-			newRating.setFood_id(food_id);
-			newRating.setRating(rating);
-			newRating.setUser_email(user_email);
-			JSONObject response;
-			try {
-				if(foodRatingRepo.getFoodRatingByUserAndFood(newRating.getUser_email(), newRating.getFood_id()) != null) {
-					throw new IllegalArgumentException();
-				}
-				foodRatingRepo.save(newRating);
-				response = generateResponse(204, HttpStatus.OK, "Food has been rated");
-			}catch (IllegalArgumentException e) {
-				response = generateResponse(400, HttpStatus.BAD_REQUEST, "Rating might already exist, or your fields are incorrect, double check your request");
-			}catch (Exception e) {
-				response = generateResponse(500, HttpStatus.INTERNAL_SERVER_ERROR, "Server might be down now. Try again");
-			}
-			return response;
-		}
-		
-		/**
-		 * edits an existing food rating
-		 * @param user_email
-		 * @param food_id
-		 * @param rating
-		 * @return a json object response
-		 */
-		@RequestMapping(method = RequestMethod.PUT, path = "/edit/{user_email}/{food_id}/{rating}", produces = MediaType.APPLICATION_JSON_VALUE)
-		@ResponseBody
-		public JSONObject editFoodRating(@PathVariable String user_email, @PathVariable int food_id, @PathVariable int rating) {
-			if(rating < 0 || rating > 5) {
-				throw new IllegalArgumentException();
-			}
-			FoodRating findOldVersion = foodRatingRepo.getFoodRatingByUserAndFood(user_email, food_id);
-			JSONObject response;
-			try {
-				if(findOldVersion == null) {
-					throw new IllegalArgumentException();
-				}
-				findOldVersion.setRating(rating);
-				foodRatingRepo.save(findOldVersion);
-				response = generateResponse(204, HttpStatus.OK, "Food rating has been edited");
-			}catch (IllegalArgumentException e) {
-				response = generateResponse(400, HttpStatus.BAD_REQUEST, "Rating might already exist, or your fields are incorrect, double check your request");
-			}catch (Exception e) {
-				response = generateResponse(500, HttpStatus.INTERNAL_SERVER_ERROR, "Server might be down now. Try again");
-			}
-			return response;
-		}
-		
-		/**
-		 * deletes an existing food rating
-		 * @param user_email
-		 * @param food_id
-		 * @return a json object response
-		 */
-		@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{user_email}/{food_id}", produces = MediaType.APPLICATION_JSON_VALUE) 
-		@ResponseBody
-		public JSONObject deleteFood(@PathVariable String user_email, @PathVariable int food_id) {
-			FoodRating oldVersion = foodRatingRepo.getFoodRatingByUserAndFood(user_email, food_id);
-			JSONObject response;
-			try {
-				if(oldVersion == null) {
-					throw new IllegalArgumentException();
-				}
-				foodRatingRepo.deleteById(oldVersion.getRating_id());
-				response = generateResponse(204, HttpStatus.OK, "Food Rating has been deleted");
-			}catch (IllegalArgumentException e) {
-				response = generateResponse(400, HttpStatus.BAD_REQUEST, "Could not find that food rating in the database, or your fields are incorrect, double check your request");
-			}catch (Exception e) {
-				response = generateResponse(500, HttpStatus.INTERNAL_SERVER_ERROR, "Server might be down now. Try again");
-			}
-			return response;
-		}
-		
-		/**
-		 * helper to generate a json object response
-		 * @param status
-		 * @param input
-		 * @param message
-		 * @return a json object response
-		 */
-		@SuppressWarnings("unchecked")
-		private JSONObject generateResponse(int status, HttpStatus input, String message) {
-			JSONObject response = new JSONObject();
-			response.put("status", status);
-			response.put("HttpStatus", input);
-			response.put("message", message);
-			return response;
-		}
+		return foodRatingService.createEntity(newRating, newRating.getRating_id());
+	}
+	
+	/**
+	 * edits an existing food rating
+	 * @param user_email
+	 * @param food_id
+	 * @param rating
+	 * @return a json object response
+	 */
+	@RequestMapping(method = RequestMethod.PUT, path = "/edit/{user_email}/{food_id}/{rating}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> editFoodRating(@PathVariable String user_email, @PathVariable int food_id, @PathVariable int rating) {
+		FoodRating toEdit = foodRatingService.getRatingForUserAndFood(user_email, food_id);
+		toEdit.setRating(rating);
+		return foodRatingService.editEntity(toEdit, toEdit.getRating_id());
+	}
+	
+	/**
+	 * deletes an existing food rating
+	 * @param user_email
+	 * @param food_id
+	 * @return a json object response
+	 */
+	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{user_email}/{food_id}", produces = MediaType.APPLICATION_JSON_VALUE) 
+	@ResponseBody
+	public ResponseEntity<?>  deleteFood(@PathVariable String user_email, @PathVariable int food_id) {
+		FoodRating toDelete = foodRatingService.getRatingForUserAndFood(user_email, food_id);
+		return foodRatingService.deleteEntityById(toDelete.getRating_id());
+	}
 }

cf6200ddc1f7229da8119231710463ddfac87e1b
unknown <jgreazel@iastate.edu>
2019-04-24 13:46:07 -0500

    javadoc done

--- Backend/business/doc/package-search-index.js
+++ Backend/business/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.business.data.controllers"},{"l":"com.example.business.data.entities"},{"l":"com.example.business.data.repositories"},{"l":"com.example.business.data.services"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.business.data.controllers"},{"l":"com.example.business.data.entities"},{"l":"com.example.business.data.services"},{"l":"com.example.business.websocket"}]

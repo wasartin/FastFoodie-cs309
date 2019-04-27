45ad1634c5ffecaf071dfcbb38cee2c299a46fc1
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-27 18:25:18 -0500

    Created enum for filtering queries

---
+++ Backend/business/src/main/java/com/example/business/page/FilterOperation.java
@@ -0,0 +1,41 @@
+package com.example.business.page;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum FilterOperation {
+	EQUAL						("eq"),
+	NOT_EQUAL					("neq"),
+	GREATER_THAN				("gt"),
+	GREATER_THAN_OR_EQUAL_TO	("gte"),
+	LESS_THAN					("lt"),
+	LESSTHAN_OR_EQUAL_TO		("lte"),
+	IN							("in"),
+	NOT_IN						("nin"),
+	BETWEEN						("btn"),
+	ASCENDING					("asc"),
+	DESCENDING					("desc"),
+	CONTAINS					("like"),
+	UNKNOWN						("");
+	
+	private String value;
+
+	FilterOperation(String value) {
+		this.value = value;
+	}
+
+	@Override
+	@JsonValue
+	public String toString() {
+		return String.valueOf(value);
+	}
+
+	public final static FilterOperation fromValue(String value){
+		for(FilterOperation op : FilterOperation.values()){
+			if(op.toString().equalsIgnoreCase(value)){
+				return op;
+			}
+		}
+		return UNKNOWN;
+	}
+
+}
